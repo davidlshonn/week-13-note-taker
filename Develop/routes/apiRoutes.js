108d1fa478eab27224cc1d9aacf04ad13e0ddf07
David Shonn <davidlshonn@icloud.com>
2021-01-24 13:58:17 +0000

    apiRoutes progress

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -1,16 +1,43 @@
 const router = require("express").Router();
-const path = require("path")
+const path = require("path");
+const fs = require("fs");
+// const store = require("../db/store");
 
-router.get("/api/notes", (req, res ) => {
-    //Should read the `db.json` file and return all saved notes as JSON
+router.get("/api/notes", (req, res) => {
+    res.json(notes);
 })
+
+router.get("/api/notes/:id", function(req, res){
+    res.json(notes[Number(req.params.id)]);
+})
+
 
 router.post("/api/notes", (req, res) => {
-    // Should receive a new note to save on the request body, add it to the `db.json` file, and then return the new note to the client
-})
+    let noteEntry = req.body;
+    let uniqueId = (data.length).toString();
+    console.log(uniqueId);
+    noteEntry.id = uniqueId;
+    data.push(noteEntry);
 
-router.delete("/api/notes/:id", (req, res) => {
-    // Should receive a query parameter containing the id of a note to delete.
-})
+    fs.writeFileSync("./db/db.json", JSON.stringify(notes), function(err){
+        if (err) throw (err);
+    });
+
+    res.json(notes);
+});
+
+// router.post("/api/notes", (req, res) => {
+//     let noteEntry = req.body;
+//     notes.push(noteEntry);
+//     updateDb();
+//     return console.log("Your note has been added: " + noteEntry.title);
+// })
+
+// Should receive a new note to save on the request body, add it to the `db.json` file, and then return the new note to the client
+
+
+// router.delete("/api/notes/:id", (req, res) => {
+//     // Should receive a query parameter containing the id of a note to delete.
+// })
 
 module.exports = router;
